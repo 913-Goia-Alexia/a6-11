a2e29e94e94cc9f56f7e1c1aace03f1ecb4b989e
Lexi <alexia.goia@stud.ubbcluj.ro>
2021-12-15 23:47:11 +0200

    viata
    i guess

--- src/repository/binary_repository.py
+++ src/repository/binary_repository.py
@@ -6,14 +6,11 @@ class BinaryRepository(Repository):
     def __init__(self, file_name):
         super().__init__()
         self._file_name = file_name
+        self.__read_entities()
 
     @property
-    def binary_data(self):
+    def data(self):
         return super().data
-
-    @binary_data.setter
-    def binary_data(self, value):
-        self.data = value
 
     def __read_entities(self):
         with open(self._file_name, "rb") as f:
@@ -26,36 +23,27 @@ class BinaryRepository(Repository):
 
     def __update_file(self):
         with open(self._file_name, "wb") as f:
-            pickle.dump(self.binary_data, f)
-            self.binary_data.clear()
+            pickle.dump(self.data, f)
+            self.data.clear()
 
     def add_item(self, obj):
-        self.__read_entities()
         super().add_item(obj)
         self.__update_file()
 
     def __getitem__(self, item):
-        self.__read_entities()
         i = super().__getitem__(item)
         self.data.clear()
         return i
 
     def __setitem__(self, index, item):
-        self.__read_entities()
         super().__setitem__(index, item)
         self.__update_file()
 
     def __delitem__(self, index):
-        self.__read_entities()
         super().__delitem__(index)
         self.__update_file()
 
     def __str__(self):
-        self.__read_entities()
         r = super().__str__()
         self.__update_file()
         return r
-
-
-# FUCK FUCK FUCK FUCK FUCK FUCK FUCK FUCK FUCK FUCK FUCK FUCK FUCK FUCK FUCK
-# :( :( :( :( :( :( :( :(
--- src/repository/file_repository.py
+++ src/repository/file_repository.py
@@ -6,6 +6,7 @@ class FileRepository(Repository):
         super().__init__()
         self._file_name = file_name
         self._entity_type = entity_type
+        self.__read_entities()
 
     def __read_entities(self):
         """
@@ -26,35 +27,25 @@ class FileRepository(Repository):
         """
         with open(self._file_name, "wt") as f:
             f.writelines([self._entity_type.make_it_string(item) for item in super().data])
-            self.data.clear()
+        self.data.clear()
 
     def add_item(self, item):
-        self.__read_entities()
         super().add_item(item)
         self.__update_file()
 
-    def __getitem__(self, item):
-        self.__read_entities()
-        i = super().__getitem__(item)
-        self.__update_file()
-        return i
+    #def __getitem__(self, item):
+      #  i = super().__getitem__(item)
+       # self.__update_file()
+       # return i
 
     def __setitem__(self, index, item):
-        self.__read_entities()
         super().__setitem__(index, item)
         self.__update_file()
 
     def __delitem__(self, index):
-        self.__read_entities()
         super().__delitem__(index)
         self.__update_file()
 
-    def __str__(self):
-        self.__read_entities()
-        r = super().__str__()
-        self.__update_file()
-        return r
-
-
-
-# am revenit
+    #def __str__(self):
+       # r = super().__str__()
+       # return r
